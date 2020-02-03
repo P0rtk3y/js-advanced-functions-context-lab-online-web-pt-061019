41391926f91d6a8c5e8a5289f9af5d3378de48ae
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-02-03 07:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ function hoursWorkedOnDate(date){
   return (outTime.hour - inTime.hour) / 100; 
 }
 
+function wagesEarnedOnDate
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
